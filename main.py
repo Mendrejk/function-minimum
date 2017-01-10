752dac96e63ddf8f935919b4901f8de433f15308
Mendrejk <bastian.lakomy@gmail.com>
2017-01-10 14:23:54 +0100

    Added basic functionality for custom minimum finding

--- main.py
+++ main.py
@@ -2,15 +2,31 @@ from math import inf, sin, cos
 from random import uniform
 
 
+def randic(dic, f, min):
+    pass
+
+
+def cusrandic(dic, f, cusmin, cusmax, amount=10000):
+    for i in amount-1:
+        dic[i] = {uniform(cusmin, cusmax), f(uniform(cusmin, cusmax))}
+
+
 def main():
 
-    mini = inf
-    which = bool(input("Do you want to find minimum using random values?"))
-
     def f(x): return ((0.5 * x ** 2) + (5 * sin(x)) + (2 * cos(x) ** 2))
+    mini = inf
 
-    if which:
-        print("Enter a range for random values to be generated from.", end='')
+    which = input("Do you want to find minimum using random values? yes/no")
+
+    if which == 'yes':
+        rands = {}
+
+        print('Enter a range for random values to be generated from. ',)
+        customrangemin = int(input('Input the minimal value, default is -10 ',) or -10)
+        customrangemax = int(input('Input the maximal value, default is 10 ',) or 10)
+        print('Random values: {}'.format(rands))
+    elif which == 'no':
+        pass
 
 
 if __name__ == '__main__':
