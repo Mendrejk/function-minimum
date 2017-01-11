12f49abbedee8dac8719511f1a0b559d3e752289
Mendrejk <bastian.lakomy@gmail.com>
2017-01-11 23:28:05 +0100

    random values minimum finding working now

--- main.py
+++ main.py
@@ -6,17 +6,28 @@ def randic(dic, f, min):
     pass
 
 
-def cusrandic(dic, f, cusmin, cusmax, amount=10000):
-    for i in amount-1:
-        dic[i] = {uniform(cusmin, cusmax), f(uniform(cusmin, cusmax))}
+def cusrandic(dic, f, cusmin, cusmax, amount=500):
+    for i in range(amount):
+        randnum = uniform(cusmin, cusmax)
+        if randnum not in dic.items():
+            dic[randnum] = f(randnum)
+        else:
+            amount += 1
+
+
+def findmin(dic):
+    mini = inf
+    for i in dic:
+        if dic[i] < mini:
+            mini = dic[i]
+    return mini
+
+
+def f(x): return ((0.5 * x ** 2) + (5 * sin(x)) + (2 * cos(x) ** 2))
 
 
 def main():
-
-    def f(x): return ((0.5 * x ** 2) + (5 * sin(x)) + (2 * cos(x) ** 2))
-    mini = inf
-
-    which = input("Do you want to find minimum using random values? yes/no")
+    which = input("Do you want to find minimum using random values? yes/no ")
 
     if which == 'yes':
         rands = {}
@@ -24,7 +35,11 @@ def main():
         print('Enter a range for random values to be generated from. ',)
         customrangemin = int(input('Input the minimal value, default is -10 ',) or -10)
         customrangemax = int(input('Input the maximal value, default is 10 ',) or 10)
+        cusrandic(rands, f, customrangemin, customrangemax)
         print('Random values: {}'.format(rands))
+        # for i in rands:
+        #     print(rands[i])
+        print('mini:', findmin(rands))
     elif which == 'no':
         pass
 
